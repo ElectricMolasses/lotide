e7e488746556ee7eaac0c6c6c753a43818d27075
Robert Beckett <robert.beckett@onion.red>
2019-10-15 02:30:03 +0000

    Implemented object comparison between primitives and confirmed working.

--- eqObjects.js
+++ eqObjects.js
@@ -7,5 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
+  if (keys1.length === keys2.length) {
+    for (let key in object1) {
+      if (object1[key] !== object2[key]) return false;
+    }
+  } else return false;
+
+  return true;
 };
+
+const obj1 = { a: '1', b: '2' };
+const obj2 = { a: '1', b: '2' };
+
+const obj3 = { a: '1', b: '2', c: '3' };
+
+assertEqual(eqObjects(obj1, obj2), true);
+assertEqual(eqObjects(obj2, obj3), false);
