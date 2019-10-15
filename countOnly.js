d99c6968d5dfc8a0078c0c112de10a9964c9b6a0
Robert Beckett <robert.beckett@onion.red>
2019-10-15 01:53:01 +0000

    Completed and tested countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let counts = {};
+
+  for (let i = 0; i < allItems.length; i++) {
+    if (itemsToCount[allItems[i]]) {
+      counts[allItems[i]] = (0 | counts[allItems[i]]);
+      counts[allItems[i]]++;
+    }
+  }
+
+  return counts;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames,
+  { "Jason": true,
+    "Karima": true,
+    "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
