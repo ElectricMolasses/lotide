f8aec640333f471a5c11918e1f813ead8d7de574
Robert Beckett <robert.beckett@onion.red>
2019-10-15 02:54:23 +0000

    Confirmed test cases working, and added util.inspect to properly display object contents.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -21,3 +21,20 @@ const eqObjects = function(object1, object2) {
 
   return true;
 };
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`😃😃😃 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`😒😒😒 Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(cd, cd2);
