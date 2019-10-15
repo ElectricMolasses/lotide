7d2fb03b3d98162f7ad6e397036d8dcdff92cf0d
Robert Beckett <robert.beckett@onion.red>
2019-10-15 02:05:23 +0000

    Created skeleton of letterPositions function and copied dependencies.

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+  } else return false;
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+
+
+  return results;
+}
