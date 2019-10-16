be7c16f92f8040ac98fde1e44ad426c3e23e5379
Robert Beckett <robert.beckett@onion.red>
2019-10-16 16:16:24 +0000

    Made eqArrays handle indefinitely nested arrays and confirmed working.

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,11 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) return false;
+      } else {
+        if (arr1[i] !== arr2[i]) return false;
+      }
     }
   } else return false;
   return true;
@@ -20,3 +24,10 @@ assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
 assertEqual(eqArrays([1, 5, 7, 3], [1, 5, 2, 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 8]), false);
 assertEqual(eqArrays([1, 2, 3], [8, 2, 3]), false);
+
+const test = [1, [1, 2], 2, [3, 4, [5, 1]]];
+const testIdentical = [1, [1, 2], 2, [3, 4, [5, 1]]];
+const test1 = [1, [1, 2], 2, [3, 4, [4, 1]]];
+
+assertEqual(eqArrays(test, testIdentical), true);
+assertEqual(eqArrays(test, test1), false);
