c676ad8e82cfb54c38e7a194f3ba274ed2fafc94
Robert Beckett <robert.beckett@onion.red>
2019-10-16 00:07:37 +0000

    Implemened and tested takeUntil.

--- takeUntil.js
+++ takeUntil.js
@@ -16,5 +16,21 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const takeUntil = function(array, callback) {
+  let results = [];
 
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) return results;
+    results.push(array[i]);
+  }
+  return results;
 };
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
