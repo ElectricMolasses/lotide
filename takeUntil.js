a880072803c86dca472530a2e08be3cd4ffe92d8
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-21 20:03:53 +0000

    added all the exports to index

--- takeUntil.js
+++ takeUntil.js
@@ -15,7 +15,7 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-
+  
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
