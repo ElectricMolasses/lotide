3f10c7373cf59ce41f9d8a4441d569c602b014c7
Robert Beckett <robert.beckett@onion.red>
2019-10-21 20:25:59 +0000

    Removed now useless test lines from tail.

--- tail.js
+++ tail.js
@@ -1,10 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const tail = function(array) {
   return array.slice(1);
 };
-
-let testArray = [5, 4, 3, 2, 1];
-let newArray = tail(testArray); 
 
 module.exports = tail;
