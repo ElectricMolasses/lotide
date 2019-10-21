b1209b7d8096f5111488d78e620b84e7fea16b48
Robert Beckett <robert.beckett@onion.red>
2019-10-21 20:11:41 +0000

    Revert "I ruined findKey."
    This reverts commit 0d7a206aa57b19ff77a1759062a03c0628707b90.

--- findKey.js
+++ findKey.js
@@ -1,1 +1,23 @@
-// I hate this code
+const assertEqual = require('./assertEqual');
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) return key;
+  }
+};
+
+let obj = {
+  1: "Cat",
+  2: 5,
+  3: 7,
+  4: 82,
+  5: true,
+  6: [1, 2, 3]
+};
+
+assertEqual(findKey(obj, x => typeof x === 'number'), '2');
+assertEqual(findKey(obj, x => x), "1");
+assertEqual(findKey(obj, x => Array.isArray(x)), "6");
+assertEqual(findKey(obj, () => false), undefined);
+
+module.exports = findKey;
